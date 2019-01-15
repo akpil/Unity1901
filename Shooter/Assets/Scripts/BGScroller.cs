d3f1065bf29aa9a3da069d2ec224d16fbebee912
Akpil <jyk0802@naver.com>
2019-01-15 13:52:22 +0900

    BG scrolling

---
+++ Shooter/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour {
+    private Rigidbody rb;
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.back * Speed;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("BGBumper"))
+        {
+            transform.position += Vector3.forward * (40.94f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Shooter/Assets/Scripts/BGTextureScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGTextureScroll : MonoBehaviour {
+
+    private Renderer rand;
+    private Material mat;
+    public float scrollSpeed;
+    // Use this for initialization
+    void Start () {
+        rand = GetComponent<Renderer>();
+        mat = rand.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float offset = Time.time * scrollSpeed;
+        mat.SetTextureOffset("_MainTex", new Vector2(0, offset));
+        //mat.mainTextureOffset += new Vector2(0, scrollSpeed) * Time.deltaTime;
+
+    }
+}
