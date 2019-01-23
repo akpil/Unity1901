ff4cbad7b80e8cccccf855461f72149582f8db64
Akpil <jyk0802@naver.com>
2019-01-23 12:55:24 +0900

    Boss HP, movement phase

--- Shooter/Assets/Scripts/BossController.cs
+++ Shooter/Assets/Scripts/BossController.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour {
 
+    public Bolt bossBoltPrefab;
+    public Transform boltPosition;
+    private Coroutine fireRoutine;
+
     private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
         StartCoroutine(MovePhase());
-	}
+        currentHP = MaxHP;
+
+    }
+
+    private IEnumerator Fire()
+    {
+        while (true)
+        {
+            Bolt newBolt = Instantiate(bossBoltPrefab);
+            newBolt.transform.position = boltPosition.position;
+
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
 
     private IEnumerator MovePhase()
     {
@@ -20,10 +37,50 @@ public class BossController : MonoBehaviour {
             yield return new WaitForSeconds(0.1f);
         }
         rb.velocity = Vector3.zero;
+
+        fireRoutine = StartCoroutine(Fire());
+        rb.velocity = transform.right * 2;
+        while (true)
+        {
+            yield return new WaitForSeconds(2f);
+            rb.velocity = Vector3.zero;
+            StopCoroutine(fireRoutine);
+
+            yield return new WaitForSeconds(0.5f);
+
+            fireRoutine = StartCoroutine(Fire());
+            rb.velocity = transform.right * -2;
+            yield return new WaitForSeconds(4f);
+            rb.velocity = Vector3.zero;
+            StopCoroutine(fireRoutine);
+
+            yield return new WaitForSeconds(0.5f);
+
+            fireRoutine = StartCoroutine(Fire());
+            rb.velocity = transform.right * 2;
+            yield return new WaitForSeconds(2f);
+        }
     }
 
-	// Update is called once per frame
-	void Update () {
+    public int MaxHP;
+    private int currentHP;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PlayerBolt"))
+        {
+            // 체력감소
+            currentHP--;
+            Debug.Log(currentHP);
+            if (currentHP <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
