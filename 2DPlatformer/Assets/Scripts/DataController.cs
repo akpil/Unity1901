f01dedbcd472ac8d8f7a1ec383ff88d92eb91521
Akpil <jyk0802@naver.com>
2019-02-12 13:52:34 +0900

    UI elemnt finish

--- 2DPlatformer/Assets/Scripts/DataController.cs
+++ 2DPlatformer/Assets/Scripts/DataController.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class DataController : MonoBehaviour
 {
-    int[] a;
-    public string[] titles;
-    public string test;
+    public Data[] datas;
     public UIElement[] elements;
+    public Sprite[] icons;
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(string.Format("{0}번째에 들어있는 자리에 {1}식의 데이터를 넣습니다.{0}",
+                                "첫", 260.ToString()));
+
         for (int i = 0; i < elements.Length; i++)
         {
-            elements[i].SetTitle(titles[i]);
+            elements[i].SetAll(icons[i], datas[i].name, datas[i].contents, datas[i].cost, datas[i].value);
         }
+    }
+
+    public void LevelUP(int id)
+    {
+        datas[id].level++;
+        datas[id].cost = datas[id].baseCost * datas[id].level;
+        datas[id].value++;
+        elements[id].Renew(datas[id].contents, datas[id].cost, datas[id].value);
     }
 
     // Update is called once per frame
@@ -22,4 +32,14 @@ public class DataController : MonoBehaviour
     {
         
     }
+}
+[System.Serializable]
+public class Data
+{
+    public string name;
+    public string contents;
+    public float cost;
+    public float baseCost;
+    public int level;
+    public int value;
 }
--- 2DPlatformer/Assets/Scripts/UIElement.cs
+++ 2DPlatformer/Assets/Scripts/UIElement.cs
@@ -16,15 +16,25 @@ public class UIElement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         //title.text = "이름공간";
-        cost.text = "가격공간";
-        contents.text = "상세설명이 들어가는 공간 두줄 이상의 데이터가 들어갈 수 있음";
-        purchase.text = "구매";
+        //cost.text = "가격공간";
+        //contents.text = "상세설명이 들어가는 공간 두줄 이상의 데이터가 들어갈 수 있음";
+        //purchase.text = "구매";
     }
 
-    public void SetTitle(string name)
+    public void SetAll(Sprite inputIcon, string name, string content, float dataCost, int value)
     {
+        icon.sprite = inputIcon;
         title.text = name;
+        contents.text = string.Format(content, value.ToString());
+        cost.text = dataCost.ToString();
+    }
+
+    public void Renew(string content, float dataCost, int value)
+    {
+        contents.text = string.Format(content, value.ToString());
+        cost.text = dataCost.ToString();
     }
 
     // Update is called once per frame
